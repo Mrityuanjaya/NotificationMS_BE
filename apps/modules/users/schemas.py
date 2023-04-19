24fe8dd239001a83d31a096696f095ac176b7ba1
Sahil Chawla <sahil.chawla@joshtechnologygroup.com>
2023-04-19 20:11:29 +0530

    edited login api according to new schema

--- apps/modules/users/schemas.py
+++ apps/modules/users/schemas.py
@@ -4,19 +4,20 @@ from tortoise import fields
 
 
 class User(Model):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=200)
-    email = fields.CharField(max_length=320)
-    is_active = fields.BooleanField(default=True)
-    created_at = fields.DatetimeField(auto_now_add=True)
-    modified_at = fields.DatetimeField(auto_now=True)
-
-
-class Admin(Model):
     class Role(IntEnum):
         SystemAdmin = 1
         Admin = 2
 
-    user = fields.ForeignKeyField("models.User", related_name="users", pk=True)
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=200)
+    email = fields.CharField(max_length=320)
     hashed_password = fields.CharField(max_length=64)
     role = fields.IntEnumField(Role)
+    is_active = fields.BooleanField(default=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
+    deleted_at = fields.DatetimeField(null=True)
+
+
+class Admin(Model):
+    user = fields.ForeignKeyField("models.User", related_name="users", pk=True)
