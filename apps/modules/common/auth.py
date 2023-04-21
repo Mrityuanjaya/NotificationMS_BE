8408a635ff6cccbb4cc7130b0638b6589d63c473
Sahil Chawla <sahil.chawla@joshtechnologygroup.com>
2023-04-21 13:47:39 +0530

    review fixes

--- apps/modules/common/auth.py
+++ apps/modules/common/auth.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+
 from jose import jwt
 
 from apps.settings.local import settings
@@ -9,10 +10,7 @@ def create_access_token(data: dict, expires_delta: timedelta) -> jwt:
     function to generate a jwt token when a user logs in.
     token will encode user's email inside it
     """
-    to_encode = data.copy()
-    expire = datetime.utcnow() + expires_delta
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(
-        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
-    )
+    expiry_time = datetime.utcnow() + expires_delta
+    data.update({"exp": expiry_time})
+    encoded_jwt = jwt.encode(data, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
